451007fb388a261609a637e236a4715d1acdcae5
Mateusz Brychczynski <mat.brych@gmail.com>
2023-08-02 15:05:21 +0100

    first commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Mateusz Brychczynski on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
